8cc59da1b13d76b0bea78554c0352b1b538b5f54
Ryan Hoang <tofu1300@gmail.com>
2023-08-17 09:20:48 -0500

    working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,7 @@ function Doan() {
       <div className='footer-doan-title footer-mobile'>
         LDDMMC
       </div>
+      <div className="footer-spacer"/>
       <div className='footer-doan-alt'>
         Our Lady of the Holy Rosary
       </div>
@@ -23,6 +24,7 @@ function Links() {
       <div className='footer-link-title'>
         Links
       </div>
+      <div className="footer-spacer"/>
       <a className='footer-link-veym' href="https://veym.net">
         VEYM Main Website
       </a>
@@ -42,6 +44,7 @@ function Contact() {
       <div className='footer-contact-title'>
         Contact Us
       </div>
+      <div className="footer-spacer"/>
       <a className='footer-contact-link' href="mailto:btvlddmmc@veym.net">
         <svg className='footer-contact-icon' viewBox="0 0 8 6" xmlns="http://www.w3.org/2000/svg">
           <path d="m0 0h8v6h-8zm.75 .75v4.5h6.5v-4.5zM0 0l4 3 4-3v1l-4 3-4-3z"/>
--- src/pages/resources/admin/PhungVu.jsx
+++ src/pages/resources/admin/PhungVu.jsx
@@ -3,7 +3,8 @@ export default function PhungVu() {
     <div className='resources-phungvu'>
       Phụng Vụ
       <embed className="resources-pdf" src="/resources/phungvu/Divine_Mercy_Chaplet_Templates.pdf" type="application/pdf"/>
-      <img className='events-image' src='/events/events.jpg' />
+      <embed className="resources-pdf" src="/resources/phungvu/Eucharistic_Adoration_Resources.pdf" type="application/pdf"/>
+      <embed className="resources-pdf" src="/resources/phungvu/Lanh_Nhan_Loi_Chua_Template.pdf" type="application/pdf"/>
     </div>
   )
 }
