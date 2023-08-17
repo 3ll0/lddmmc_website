04f1da10299640f86ba893be7b55e94fd54dd85e
Ryan Hoang <tofu1300@gmail.com>
2023-08-17 00:32:08 -0500

    working

--- src/pages/resources/admin/PhungVu.jsx
+++ src/pages/resources/admin/PhungVu.jsx
@@ -2,6 +2,8 @@ export default function PhungVu() {
   return (
     <div className='resources-phungvu'>
       Phụng Vụ
+      <embed className="resources-pdf" src="/resources/phungvu/Divine_Mercy_Chaplet_Templates.pdf" type="application/pdf"/>
+      <img className='events-image' src='/events/events.jpg' />
     </div>
   )
 }
