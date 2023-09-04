2fe271365a6067bb858630d539b671ccab69068b
ryan <tofu1300@gmail.com>
2023-09-03 21:12:40 -0500

    working

--- src/pages/resources/other/DHLD.jsx
+++ src/pages/resources/other/DHLD.jsx
@@ -17,6 +17,11 @@ export default function DHLD() {
         <div className="resources-dhld-downloads-files">
           <a className="resources-dhld-downloads-file" href="/resources/dhld/DHLD 2023 - Sports Rules .pdf.pdf" download>Sports Rules</a>
           <a className="resources-dhld-downloads-file" href="/resources/dhld/DHLD 2023 - Van Nghe Rules .pdf" download>Van Nghe Rules</a>
+          <a className="resources-dhld-downloads-file" href="/resources/dhld/1. Thong Bao DHLD 2023.pdf" download>Thong Bao DHLD 2023</a>
+          <a className="resources-dhld-downloads-file" href="/resources/dhld/2. Don Ghi Danh DHLD 2023.pdf" download>Don Ghi Danh DHLD 2023</a>
+          <a className="resources-dhld-downloads-file" href="/resources/dhld/3. VEYM-Participant-Agreement-Form Updated 9-20-2020.pdf" download>VEYM-Participant-Agreement-Form</a>
+          <a className="resources-dhld-downloads-file" href="/resources/dhld/4. DHLD 2023 Registration Instructions.pdf" download>DHLD 2023 Registration Instructions</a>
+          <a className="resources-dhld-downloads-file" href="/resources/dhld/Registration - Online Submission.xlsx" download>Registration - Online Submission</a>
         </div>
       </div>
     </div>
